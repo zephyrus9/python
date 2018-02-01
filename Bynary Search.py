2e89dca1ead97e2717bbb39e98e7ab0ba6596232
sheng <xsleecumt@outlook.com>
2018-02-01 22:34:53 +0800

    2

---
+++ Bynary Search.py
@@ -0,0 +1,18 @@
+def search(sequence, number, lower=0, upper=None):
+
+    if upper == None:
+        upper = len(sequence)
+    if upper == lower:
+        assert number == sequence[upper]
+        return upper
+    else:
+        middle = (upper + lower) // 2
+        if number > sequence[middle]:
+            return search(sequence, number, middle, upper)
+        else:
+            return search(sequence, numbwe, lower, middle - 1)
+
+if __name__ == '__main__':
+    seq = list(range(1:100:2))
+    search(seq, 34)
+
