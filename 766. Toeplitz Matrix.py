1168ad5faa1c777c7f43fc2e9c67588c4acda081
sheng <xsleecumt@outlook.com>
2018-02-05 20:22:04 +0800

    766

--- 766. Toeplitz Matrix.py
+++ 766. Toeplitz Matrix.py
@@ -1,14 +1,18 @@
 # -*-coding: utf-8 -*-
 # Author: 
 
-class Solution:
 
+class Solution:
+    """
+
+    """
     def isToeplitzMatrix(self, matrix):
         for i in range(len(matrix)-1):
             for j in range(len(matrix[0])-1):
                 if matrix[i][j] == matrix[i+1][j+1]:
                     return True
         return False
+
 
 if __name__ == '__main__':
     matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
