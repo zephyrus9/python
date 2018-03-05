39ec33a6e38dea7569028a1e95f0715d5f4d6fa0
sheng <xsleecumt@outlook.com>
2018-03-05 18:00:20 +0800

    selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,2 +1,23 @@
 # -*-coding: utf-8 -*-
-# Author: 
+# Author:
+
+""" 选择排序"""
+
+
+def selection_sort(alist):
+    for fill_slot in range(len(alist) - 1, 0, -1):
+        pos_of_max = 0
+        for location in range(1, fill_slot + 1):
+            if alist[location] > alist[pos_of_max]:
+                pos_of_max = location
+
+        # sort
+        temp = alist[fill_slot]
+        alist[fill_slot] = alist[pos_of_max]
+        alist[pos_of_max] = temp
+    return alist
+
+alist3 = [2,3,2,33,24,54,23,112,31]
+selection_sort(alist3)
+print("选择排序：")
+print(alist3)
