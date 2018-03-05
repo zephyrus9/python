9c76b2335f92f6e95c31047fb34be423487ed01b
sheng <xsleecumt@outlook.com>
2018-03-05 18:01:48 +0800

    sort

--- mySort.py
+++ mySort.py
@@ -35,3 +35,28 @@ alist2 = [2,3,2,33,24,54,23,112,31]
 shortBubbleSort(alist2)
 print("短冒泡排序：")
 print(alist2)
+
+
+# -*-coding: utf-8 -*-
+# Author:
+
+""" 选择排序 """
+# 时间复杂度与冒泡一样都是O(n^2)，但是交换的次数减少很多，所以会更快的执行；
+
+def selection_sort(alist):
+    for fill_slot in range(len(alist) - 1, 0, -1):
+        pos_of_max = 0
+        for location in range(1, fill_slot + 1):
+            if alist[location] > alist[pos_of_max]:
+                pos_of_max = location
+
+        # sort
+        temp = alist[fill_slot]
+        alist[fill_slot] = alist[pos_of_max]
+        alist[pos_of_max] = temp
+    return alist
+
+alist3 = [2,3,2,33,24,54,23,112,31]
+selection_sort(alist3)
+print("选择排序：")
+print(alist3)
