3e3b7e802c61aa714f0867d345d265a5467ea128
sheng <xsleecumt@outlook.com>
2018-03-07 20:58:30 +0800

    sort

--- mySort.py
+++ mySort.py
@@ -60,3 +60,62 @@ alist3 = [2,3,2,33,24,54,23,112,31]
 selection_sort(alist3)
 print("选择排序：")
 print(alist3)
+
+
+""" 插入排序 """
+
+
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+        currentvalue = alist[index]
+        position = index
+
+        while position > 0 and alist[position - 1] > currentvalue:
+            alist[position] = alist[position - 1]
+            position = position - 1
+
+        alist[position] = currentvalue
+
+alist4 = [2,3,2,33,24,54,23,112,31]
+insertionSort(alist4)
+print("插入排序： ")
+print(alist4)
+
+
+" 归并排序 "
+def merge_sort(a_list):
+    # splitting the list
+    print("Spitting", a_list)
+    if len(a_list) > 1:
+        mid = len(a_list) // 2
+        left_half = a_list[:mid]
+        right_half = a_list[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                a_list[k] = left_half[i]
+                i += 1
+            else:
+                a_list[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            a_list[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            j += 1
+            k += 1
+
+    print("Merging", a_list)
+
+alist5 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+merge_sort(alist5)
+print(alist5)
