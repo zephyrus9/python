3a55facb13b5d315937d676dc35dc96d31734f3f
sheng <xsleecumt@outlook.com>
2018-03-11 17:51:54 +0800

    graph

---
+++ Algorithms/graphs/Graph.py
@@ -0,0 +1,45 @@
+# -*-coding: utf-8 -*-
+# Author:
+
+
+from graphs.Vertex import Vertex
+
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices = self.numVertices + 1
+        newVertex = Vertex(key) # create a new vertex
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def __contains__(self, item):
+        return item in self.vertList
+
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(t)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+            self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+        def getVertices(self):
+            return self.vertList.keys()
+
+        def __iter__(self):
+            return iter(self.vertList.values())
+
+if __name__ == '__main__':
+    g = Graph()
+    for i in range(6):
+        g.addVertex(i)
+    print(g.vertList)
---
+++ Algorithms/graphs/wordladdar.py
@@ -0,0 +1,27 @@
+# -*-coding: utf-8 -*-
+# Author: 
+from Graph import Graph
+
+
+def buildGraph(wordFile):
+    d = {}
+    g = Graph()
+    wfile = open(wordFile, 'r')
+    # create buckets of words that differ by one letter
+    for line in wfile:
+        word = line[:-1]
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i+1:]
+            if bucket in d:
+                d[bucket].append(word)
+            else:
+                d[bucket] = [word]
+
+    for bucket in d.keys():
+        for word1 in d[bucket]:
+            for word2 in d[bucket]:
+                if word1 != word2:
+                    g.addEdge(word1, word2)
+    return g
+
+buildGraph('wordFile.txt')
