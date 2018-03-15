ece4e8c911e281ad24f6b84962d77cdea3d16eec
sheng <xsleecumt@outlook.com>
2018-03-15 22:07:52 +0800

    hash

--- Algorithms/myHash.py
+++ Algorithms/myHash.py
@@ -1,2 +1,16 @@
 # -*-coding: utf-8 -*-
 # Author: 
+
+
+voted = {}
+def check_voter(name):
+    if voted.get(name):
+        print('Kick ' + name + ' out')
+    else:
+        voted[name] = True
+        print("let them vote!")
+
+
+check_voter('tom')
+check_voter('jane')
+check_voter('tom')
